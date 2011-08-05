22ec108691e609e834672196d659fdc32ab7da79
elenilson.vieira.filho <elenilson.vieira.filho@gmail.com>
2011-08-05 00:52:57 +0000

    Iniciando o projeto.

---
+++ ERP.Lavanderia/ERP.Lavanderia.Module.Web/WebModule.cs
@@ -0,0 +1,16 @@
+using System;
+using System.ComponentModel;
+
+using DevExpress.ExpressApp;
+
+namespace ERP.Lavanderia.Module.Web
+{
+    [ToolboxItemFilter("Xaf.Platform.Web")]
+    public sealed partial class LavanderiaAspNetModule : ModuleBase
+    {
+        public LavanderiaAspNetModule()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ERP.Lavanderia/ERP.Lavanderia.Module/Module.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+
+using DevExpress.ExpressApp;
+using System.Reflection;
+
+
+namespace ERP.Lavanderia.Module
+{
+    public sealed partial class LavanderiaModule : ModuleBase
+    {
+        public LavanderiaModule()
+        {
+            InitializeComponent();
+        }
+    }
+}
