e3ef9b064829fdaaeb424cfc6f6527e6b607e4ac
elenilson.vieira.filho@gmail.com <elenilson.vieira.filho@gmail.com@fb07881e-072b-54e8-65b5-7de15626595c>
2011-08-09 02:50:51 +0000

    Criado o filtro para a listview de lavagem, mas o filtro ta sendo aplicado no início e no menu Lavagem.

---
+++ trunk/ERP.Lavanderia/ERP.Lavanderia.Module/PacoteControladoress/ControladoresLavagem/LavagensDoDiaViewController.cs
@@ -0,0 +1,40 @@
+using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Actions;
+using DevExpress.Persistent.Base;
+using DevExpress.Xpo;
+using ERP.Lavanderia.Module.PacoteLavagem;
+using DevExpress.Data.Filtering;
+
+namespace ERP.Lavanderia.Module.PacoteControladoress.ControladoresLavagem
+{
+    public partial class LavagensDoDiaViewController : ViewController
+    {
+        public LavagensDoDiaViewController()
+        {
+            InitializeComponent();
+            RegisterActions(components);
+        }
+
+        protected override void OnActivated()
+        {
+            base.OnActivated();
+            View.ControlsCreated += new EventHandler(View_ControlsCreated);
+        }
+
+        void View_ControlsCreated(object sender, EventArgs e)
+        {
+            var now = DateTime.Now;
+            var limiteInferior = new DateTime(now.Year, now.Month, now.Day);
+            var limiteSuperior = new DateTime(now.Year, now.Month, now.Day, 23, 59, 59);
+
+            //((ListView)View).CollectionSource.Criteria["Filter1"] = new BetweenOperator("DataHoraPreferivelParaEntrega", limiteInferior, limiteSuperior);
+
+        }
+    }
+}
