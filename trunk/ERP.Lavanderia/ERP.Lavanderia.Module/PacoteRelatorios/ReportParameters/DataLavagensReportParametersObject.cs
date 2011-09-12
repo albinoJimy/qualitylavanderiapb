a0cbba8f254faee01fa4d719f01a1e7622bff559
elenilson.vieira.filho@gmail.com <elenilson.vieira.filho@gmail.com@fb07881e-072b-54e8-65b5-7de15626595c>
2011-09-12 00:50:47 +0000

    Criado o filtro de lavagem por dia.

---
+++ trunk/ERP.Lavanderia/ERP.Lavanderia.Module/PacoteRelatorios/ReportParameters/DataLavagensReportParametersObject.cs
@@ -0,0 +1,49 @@
+using System;
+
+using DevExpress.Xpo;
+using DevExpress.Xpo.DB;
+using DevExpress.Data.Filtering;
+
+using DevExpress.ExpressApp;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.Persistent.Validation;
+using DevExpress.ExpressApp.Reports;
+using ERP.Lavanderia.Module.PacoteConfiguracoes;
+
+namespace ERP.Lavanderia.Module.PacoteRelatorios.ReportParameters
+{
+    [NonPersistent]
+    public class DataLavagensReportParametersObject : ReportParametersObjectBase
+    {
+        private DateTime data;
+
+        public DataLavagensReportParametersObject(Session session) : base(session) { }
+
+        [RuleRequiredField("RuleRequiredField DataLavagensReportParametersObject.Data", "PreviewReport", "Data não pode ser vazia")]
+        public DateTime Data
+        {
+            get { return data; }
+            set { SetPropertyValue("DataInicial", ref data, value); }
+        }
+
+        public override void AfterConstruction()
+        {
+            Data = ConfiguracaoGeral.RetornaConfiguracaoGeral(Session).DataHoraAtual;
+
+            base.AfterConstruction();
+        }
+
+        public override CriteriaOperator GetCriteria()
+        {
+            return new BinaryOperator("DataHoraDeRecebimento", data);
+        }
+
+        public override SortingCollection GetSorting()
+        {
+            SortingCollection sorting = new SortingCollection();
+            return sorting;
+        }
+    }
+
+}
