590230f362731da46492e8ce021050c2ca89f195
elenilson.vieira.filho <elenilson.vieira.filho@gmail.com>
2011-09-12 01:34:59 +0000

    Ajeitado a versão.

--- ERP.Lavanderia/ERP.Lavanderia.Module.Web/Properties/AssemblyInfo.cs
+++ ERP.Lavanderia/ERP.Lavanderia.Module.Web/Properties/AssemblyInfo.cs
@@ -28,6 +28,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.1.60")]
+[assembly: AssemblyVersion("1.2.3")]
 
-[assembly: AssemblyFileVersionAttribute("1.1.60")]
+[assembly: AssemblyFileVersionAttribute("1.2.3")]
